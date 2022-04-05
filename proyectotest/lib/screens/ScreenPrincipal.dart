13564f42da3a1f0e60a9387af263ef18f7284516
YamilRG <eduardo_1600@hotmail.com>
2022-04-05 09:10:42 -0500

    ScreenPrincipal

---
+++ proyectotest/lib/screens/ScreenPrincipal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ScreenPrincipal extends StatefulWidget {
+  const ScreenPrincipal({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenPrincipal> createState() => _ScreenPrincipalState();
+}
+
+class _ScreenPrincipalState extends State<ScreenPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
